a37d50a7a9a46f3db123aedae8e82cfd04126ce0
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-03 16:03:01 -0700

    add state to toggle modal

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import Button from './components/atoms/Button';
+import FriendsModal from './components/organisms/FriendsModal';
 
 export default function App() {
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const viewFriends = () => {
+    setToggleModal(!toggleModal)
+  }
+
+// toggle button, set modal to true when touched, pass props to modal so that modal knows
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {(toggleModal) ? <FriendsModal /> : <Text>Invite Your Friends!</Text>}
+      <Button text="Invite Friends" press={viewFriends}/>
       <StatusBar style="auto" />
     </View>
   );
